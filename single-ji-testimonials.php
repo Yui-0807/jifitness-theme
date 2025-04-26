f23ddce9e83e1b900b4af7d8eb51071a1cfaa889
shiangchinlin <150076701+shiangchinlin@users.noreply.github.com>
2025-04-25 20:25:30 -0700

    add single-ji-testimonials.php

---
+++ single-ji-testimonials.php
@@ -0,0 +1,28 @@
+<?php
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+
+		<?php
+		while ( have_posts() ) :
+			the_post();
+
+			get_template_part( 'template-parts/content', get_post_type() );
+
+			the_post_navigation(
+				array(
+					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'ji' ) . '</span> <span class="nav-title">%title</span>',
+					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'ji' ) . '</span> <span class="nav-title">%title</span>',
+				)
+			);
+
+
+		endwhile; // End of the loop.
+		?>
+
+	</main><!-- #primary -->
+
+<?php
+
+get_footer();

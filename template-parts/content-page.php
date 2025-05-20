4295cbf6cc717604cfb8b871835403f34a40688d
Yui-0807 <131344066+Yui-0807@users.noreply.github.com>
2025-05-21 00:29:28 +0800

    adjusted html structure

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -10,12 +10,12 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
+	<div class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
-
-	<?php jifitness_post_thumbnail(); ?>
-
+		<?php jifitness_post_thumbnail(); ?>
+	</div><!-- .entry-header -->
+		
+		
 	<div class="entry-content">
 		<?php
 		the_content();

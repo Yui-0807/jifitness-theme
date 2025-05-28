9928fb6d3d217477ff643f36bf2a92f810164d37
shiangchinlin <150076701+shiangchinlin@users.noreply.github.com>
2025-05-27 17:06:02 -0700

    update core value

---
+++ images/passionate-coaching-icon.php
@@ -0,0 +1,7 @@
+<svg xmlns="http://www.w3.org/2000/svg" width="54" height="54" viewBox="0 0 54 54" fill="none">
+  <path d="M19.0137 13.1196C20.5977 12.5696 22.3977 12.2796 24.2997 12.2796C26.8137 12.2796 29.2137 12.7196 31.2777 13.4896" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M39.7017 14.9489C43.6857 17.9489 46.3977 22.7799 46.3977 28.1699C46.3977 37.7499 38.8477 45.2999 29.2677 45.2999C23.8777 45.2999 19.0477 42.5879 16.0477 38.6039" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M10.9998 28.1698C10.9998 27.5998 11.0318 27.0358 11.0918 26.4798" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M26.9998 6.69995L22.4998 11.1999L26.9998 15.6999" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M8.99988 18.2999L13.4999 22.7999L17.9999 18.2999" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>

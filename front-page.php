343598e3bcf5c1a153966dcc0f60014fab713e41
shiangchinlin <150076701+shiangchinlin@users.noreply.github.com>
2025-05-29 00:09:27 -0700

    update front page swiper

--- front-page.php
+++ front-page.php
@@ -16,54 +16,54 @@ get_header();
 
 
 <section class="banner">
-  <?php if ( have_rows( 'page_banner' ) ) : ?>
-    <div class="swiper banner-swiper">
-      <div class="swiper-wrapper">
+    <?php if ( have_rows( 'page_banner' ) ) : ?>
+      <div class="swiper banner-swiper">
+        <div class="swiper-wrapper">
 
-        <?php while ( have_rows( 'page_banner' ) ) : the_row(); 
-          $heading     = get_sub_field( 'heading' );
-          $description = get_sub_field( 'description' );
-          $view_all    = get_sub_field( 'view_all' );
-          $image       = get_sub_field( 'image' );
-        ?>
-          <div class="swiper-slide banner-content">
-            <div class="content-background">
-              <?php if ( $heading ) : ?>
-                <h2><?php echo esc_html( $heading ); ?></h2>
-              <?php endif; ?>
+          <?php while ( have_rows( 'page_banner' ) ) : the_row(); 
+            $heading     = get_sub_field( 'heading' );
+            $description = get_sub_field( 'description' );
+            $view_all    = get_sub_field( 'view_all' );
+            $image       = get_sub_field( 'image' );
+          ?>
+            <div class="swiper-slide banner-content">
+              <div class="content-background">
+                <?php if ( $heading ) : ?>
+                  <h2><?php echo esc_html( $heading ); ?></h2>
+                <?php endif; ?>
 
-              <?php if ( $description ) : ?>
-                <p><?php echo esc_html( $description ); ?></p>
-              <?php endif; ?>
+                <?php if ( $description ) : ?>
+                  <p><?php echo esc_html( $description ); ?></p>
+                <?php endif; ?>
 
-              <?php if ( ! empty( $view_all['url'] ) ) : ?>
-                <a href="<?php echo esc_url( $view_all['url'] ); ?>">View All</a>
+                <?php if ( ! empty( $view_all['url'] ) ) : ?>
+                  <a href="<?php echo esc_url( $view_all['url'] ); ?>">View All</a>
+                <?php endif; ?>
+              </div>
+
+              <?php if ( $image ) : ?>
+                <?php 
+                  echo wp_get_attachment_image(
+                    $image['ID'], 
+                    'full', 
+                    false, 
+                    array( 
+                      'alt'   => esc_attr( $image['alt'] ?? '' ), 
+                      'class' => 'banner-image' 
+                    ) 
+                  ); 
+                ?>
               <?php endif; ?>
             </div>
+          <?php endwhile; ?>
 
-            <?php if ( $image ) : ?>
-              <?php 
-                echo wp_get_attachment_image(
-                  $image['ID'], 
-                  'full', 
-                  false, 
-                  array( 
-                    'alt'   => esc_attr( $image['alt'] ?? '' ), 
-                    'class' => 'banner-image' 
-                  ) 
-                ); 
-              ?>
-            <?php endif; ?>
-          </div>
-        <?php endwhile; ?>
-
+        </div>
+        <div class="swiper-pagination"></div>
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-button-next"></div>
       </div>
-      <div class="swiper-pagination"></div>
-      <div class="swiper-button-prev"></div>
-      <div class="swiper-button-next"></div>
-    </div>
-  <?php endif; ?>
-</section>
+    <?php endif; ?>
+  </section>
 
 <!-- manifesto -->
 <?php

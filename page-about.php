56cced14c294c9da18b02bec82f8a386bfff0578
shiangchinlin <150076701+shiangchinlin@users.noreply.github.com>
2025-06-05 15:12:03 -0700

    add page about contact section

--- page-about.php
+++ page-about.php
@@ -3,7 +3,6 @@
  * The template for displaying the about-us page
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
  * @package Jifitness_Theme
  */
 
@@ -23,78 +22,83 @@ get_header();
     <?php get_template_part( 'template-parts/content', 'manifesto' ); ?>
 
     <?php
-        $core_values = get_field('core_value_group');
-        if ($core_values) :
-        ?>
+    $core_values = get_field('core_value_group');
+    if ( $core_values ) :
+    ?>
         <section class="core-values-section">
             <div class="container">
                 <h2 class="core-value-heading"><?php echo esc_html( get_field('core_value_heading') ); ?></h2>
 
                 <div class="core-values-grid">
-                    <?php for ($i = 1; $i <= 3; $i++) :
+                    <?php for ( $i = 1; $i <= 3; $i++ ) :
                         $group_key = 'core_value_' . $i;
-                        if (!isset($core_values[$group_key])) continue;
-                        $value = $core_values[$group_key];
-                        $bg_image = $value[$group_key . '_image'];
-                        $icon     = $value[$group_key . '_icon'];
-                        $title    = $value[$group_key . '_title'];
-                        $text     = $value[$group_key . '_text'];
+                        if ( ! isset( $core_values[ $group_key ] ) ) continue;
+
+                        $value    = $core_values[ $group_key ];
+                        $bg_image = $value[ $group_key . '_image' ];
+                        $icon     = $value[ $group_key . '_icon' ];
+                        $title    = $value[ $group_key . '_title' ];
+                        $text     = $value[ $group_key . '_text' ];
                     ?>
-                        <div class="core-value-item" style="background-image: url('<?php echo esc_url($bg_image['url']); ?>')">
-                            <?php if ($icon) : ?>
-                                <img class="core-value-icon" src="<?php echo esc_url($icon['url']); ?>" alt="<?php echo esc_attr($icon['alt']); ?>" />
+                        <div class="core-value-item" style="background-image: url('<?php echo esc_url( $bg_image['url'] ); ?>')">
+                            <?php if ( $icon ) : ?>
+                                <img class="core-value-icon" src="<?php echo esc_url( $icon['url'] ); ?>" alt="<?php echo esc_attr( $icon['alt'] ); ?>" />
                             <?php endif; ?>
-                            <h3 class="core-value-title"><?php echo esc_html($title); ?></h3>
-                            <p class="core-value-text"><?php echo esc_html($text); ?></p>
+                            <h3 class="core-value-title"><?php echo esc_html( $title ); ?></h3>
+                            <p class="core-value-text"><?php echo esc_html( $text ); ?></p>
                         </div>
                     <?php endfor; ?>
                 </div>
             </div>
         </section>
-        <?php endif; ?>
+    <?php endif; ?>
 
     <?php get_template_part( 'template-parts/content', 'team' ); ?>
-
     <?php get_template_part( 'template-parts/content', 'studio-environment' ); ?>
-
     <?php get_template_part( 'template-parts/content', 'facility' ); ?>
-
     <?php get_template_part( 'template-parts/content', 'rental-pricing' ); ?>
 
-    <?php get_template_part( 'template-parts/content', 'contact' ); ?>
+    <section class="contact-section">
+        <div class="contact-wrapper">
+            <div class="contact-left">
+                <?php get_template_part( 'template-parts/content', 'contact' ); ?>
+            </div>
+            <div class="contact-right">
+                <?php
+                $contact_image_id  = get_field('contact_image');
+                $contact_image_alt = $contact_image_id ? get_post_meta( $contact_image_id, '_wp_attachment_image_alt', true ) : '';
+                ?>
+                <div class="contact-image-wrapper">
+                    <?php if ( $contact_image_id ) : ?>
+                        <?php echo wp_get_attachment_image( $contact_image_id, 'large', false, [
+                            'alt'    => esc_attr( $contact_image_alt ),
+                            'class'  => 'responsive-contact-image',
+                            'loading'=> 'lazy',
+                        ] ); ?>
+                    <?php endif; ?>
 
-    <!-- studio icon -->
+                    <div class="contact-words">
+                        <h3>HEALTH<br>CONFIDENCE<br>BEAUTY</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
-    <?php
-
-    // Check rows exists.
-    if( have_rows('studio_description') ):
-
-        // Loop through rows.
-        while( have_rows('studio_description') ) : the_row();
-
-            // Load sub field value.
-            $icon = get_sub_field('studio_icon');
-            // Do something, but make sure you escape the value if outputting directly...
-            
-            if ($icon){
+    <!-- Studio Icon -->
+    <?php if ( have_rows( 'studio_description' ) ) : ?>
+        <?php while ( have_rows( 'studio_description' ) ) : the_row(); ?>
+            <?php
+            $icon = get_sub_field( 'studio_icon' );
+            if ( $icon ) :
                 echo '<div class="studio-icon">';
-                echo ($icon);
+                echo $icon;
                 echo '</div>';
-            } 
-        // End loop.
-        endwhile;
+            endif;
+            ?>
+        <?php endwhile; ?>
+    <?php endif; ?>
 
-    // No value.
-    else :
-        // Do something...
-    endif;
-    
-    ?>
-            
+</main>
 
-    </main>
-
-<?php
-
-get_footer();
+<?php get_footer(); ?>

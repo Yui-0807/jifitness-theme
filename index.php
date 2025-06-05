fa0850ab8a16bf9002c9403fd66434896dac567e
Yui-0807 <131344066+Yui-0807@users.noreply.github.com>
2025-06-04 17:29:00 -0700

    restructured blog post

--- index.php
+++ index.php
@@ -17,17 +17,11 @@ get_header();
 
 	<main id="primary" class="site-main">
 		
-
-		<?php
-		if ( have_posts() ) :
-
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-				<?php
-			endif;?>
+		<?php if ( is_home() && ! is_front_page() ) :?>
+			<header>
+				<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+			</header>
+		<?php endif; ?>
 
 		<!-- index.php - Banner -->
 		<?php
@@ -38,24 +32,27 @@ get_header();
 
 		<?php			
 			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();?>
+			if ( have_posts() ) :
+			while ( have_posts() ) : the_post();?>
 
 				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 					<a href="<?php the_permalink(); ?>">
 						<div class="entry-header">
 							<?php the_post_thumbnail( 'landscape-blog' ); ?>
-							<p class="date"><?php echo get_the_date(); ?> </p>
-							<h2><?php the_title(); ?></h2>
 						</div><!-- .entry-header -->
 
 						<div class="entry-content">
+							<p class="date"><?php echo get_the_date(); ?> </p>
+							<h2><?php the_title(); ?></h2>
+							
+							<hr />
 							<?php the_excerpt(); ?>
+
+							<button class="read-post-link">繼續閱讀
+								<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M647-440H160v-80h487L423-744l57-56 320 320-320 320-57-56 224-224Z"/></svg>
+							</button>
 						</div><!-- .entry-content -->	
 
-						<button class="read_post_link">
-							<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M647-440H160v-80h487L423-744l57-56 320 320-320 320-57-56 224-224Z"/></svg>
-						</button>
 					</a>
 				</article><!-- #post-<?php the_ID(); ?> -->
 			<?php endwhile;

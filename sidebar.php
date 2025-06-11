2fb23f3dcbbedfa714eb2e6b3619d61d84d6ddce
Yui-0807 <131344066+Yui-0807@users.noreply.github.com>
2025-06-11 11:07:31 -0700

    added sticky posts

--- sidebar.php
+++ sidebar.php
@@ -13,5 +13,34 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 ?>
 
 <aside id="secondary" class="widget-area">
+	<?php
+		$sticky = get_option( 'sticky_posts' );
+
+		if ( ! empty( $sticky ) ) {
+			$sticky_query = new WP_Query( array(
+				'post__in'            => $sticky,
+				'orderby'             => 'post__in',  // 按照 ID 陣列順序排序
+				'ignore_sticky_posts' => 1,
+				'posts_per_page'      => 3,
+			) );
+
+			if ( $sticky_query->have_posts() ) :
+				?>
+				<section class="widget widget_sticky_posts">
+					<h2 class="widget-title">精選文章</h2>
+					<ul>
+						<?php while ( $sticky_query->have_posts() ) : $sticky_query->the_post(); ?>
+							<li>
+								<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+							</li>
+						<?php endwhile; ?>
+					</ul>
+				</section>
+				<?php
+				wp_reset_postdata();
+			endif;
+		}
+	?>
+
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
 </aside><!-- #secondary -->

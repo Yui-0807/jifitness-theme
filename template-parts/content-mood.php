029d1ce301fab5ca40201695688cd183b49a24fe
shiangchinlin <150076701+shiangchinlin@users.noreply.github.com>
2025-06-17 16:39:08 -0700

    added content mood and banner to front page

---
+++ template-parts/content-mood.php
@@ -0,0 +1,24 @@
+<?php
+$image = get_field('mood_image');
+$headline_en = get_field('mood_headline_en');
+$quote_en = get_field('mood_quote_en');
+$quote_zh = get_field('mood_quote_zh');
+?>
+
+<?php if ($image): ?>
+<section class="mood-section" style="background-image: url('<?php echo esc_url($image['url']); ?>');">
+  <div class="mood-overlay">
+    <?php if ($headline_en): ?>
+      <h2><?php echo esc_html($headline_en); ?></h2>
+    <?php endif; ?>
+
+    <?php if ($quote_en): ?>
+      <p class="quote-en">"<?php echo esc_html($quote_en); ?>"</p>
+    <?php endif; ?>
+
+    <?php if ($quote_zh): ?>
+      <p class="quote-zh"><?php echo esc_html($quote_zh); ?></p>
+    <?php endif; ?>
+  </div>
+</section>
+<?php endif; ?>

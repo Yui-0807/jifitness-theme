d7246d630835d60209a39d333b8e3253564c038b
Yui-0807 <131344066+Yui-0807@users.noreply.github.com>
2025-06-17 22:32:19 -0700

    updated front-page classes info and blog btns

--- front-page.php
+++ front-page.php
@@ -28,176 +28,56 @@ get_template_part( 'template-parts/content', 'manifesto' );
 <section>
 <h2>課程介紹</h2>
 <p>Fitness Classes</p>
+<?php 
+$class_groups = ['home_page_class_info_1', 'home_page_class_info_2', 'home_page_class_info_3'];
 
-<?php if( have_rows('home_page_class_info_1') ): ?>
-  <?php while( have_rows('home_page_class_info_1') ): the_row(); 
+foreach( $class_groups as $group_name ):
+  if( have_rows($group_name) ):
+    while( have_rows($group_name) ): the_row();
 
-    $one_on_one_class_description = get_sub_field('1_on_1_class_description');
-    $small_group_link = get_sub_field('small_group_link');
-    
-    // 一對一課程
-    if( $one_on_one_class_description instanceof WP_Post ):
-      $post = $one_on_one_class_description; // 直接指定
-      setup_postdata($post);
-      $post_id = get_the_ID();
-      ?>
-      <article id="home-post-<?php the_ID(); ?>" <?php post_class();?>>
-        <!-- class card image -->
-        <?php
-        $image = get_field('class_image');
-        if ( $image ): ?>
-            <img src="<?php echo esc_url( $image['url'] ); ?>" alt="<?php echo esc_attr( $image['alt'] ); ?>" style="max-width:300px;" />
-        <?php endif; ?>
+      $one_on_one = get_sub_field('1_on_1_class_description');
+      $small_group = get_sub_field('small_group_link');
 
-        <!-- class card content -->
-        <h3><?php the_title(); ?></h3>
-        <?php if ( get_field('class_description') ): ?>
-          <p class="home-class-description">
-            <?php
-              $class_description = get_field('class_description');
-              $text_only = strip_tags($class_description);
-              $short_description = wp_trim_words($text_only, 60, '...');
-              echo '<div class="class-description">' . $short_description . '</div>';
-            ?>
-          </p>
-        <?php endif; ?>
+      // 一對一課程
+      if( $one_on_one instanceof WP_Post ):
+        setup_postdata($one_on_one);
+        $image = get_field('class_image', $one_on_one->ID);
+        $desc = get_field('class_description', $one_on_one->ID);
+        ?>
+        <article id="home-post-<?php echo $one_on_one->ID; ?>" <?php post_class('', $one_on_one->ID); ?>>
+          <?php if ( $image ): ?>
+            <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+          <?php endif; ?>
+          <h3><?php echo get_the_title($one_on_one); ?></h3>
+          <?php if ($desc): ?>
+            <p class="home-class-description">
+              <div class="class-description">
+                <?php echo wp_trim_words(strip_tags($desc), 60, '...'); ?>
+              </div>
+            </p>
+          <?php endif; ?>
+          <a class="default-btn" href="<?php echo esc_url(home_url('/1-on-1#1-on-1-' . $one_on_one->ID)); ?>">
+            一對一課程
+          </a>
+        </article>
+        <?php wp_reset_postdata(); ?>
+      <?php endif; ?>
 
-        <a class="default-btn" href="<?php echo esc_url(home_url('/1-on-1#1-on-1-' . $post_id)); ?>">
-          一對一課程
+      <!-- 團體課程按鈕 -->
+      <?php if( $small_group instanceof WP_Post ): ?>
+        <?php setup_postdata($small_group); ?>
+        <a class="default-btn" href="<?php echo esc_url(home_url('/small-group#small-group-' . $small_group->ID)); ?>">
+          團體課程
         </a>
-      </article>
-      <?php wp_reset_postdata(); ?>
-    <?php endif; ?>
+        <?php wp_reset_postdata(); ?>
+      <?php endif; ?>
+    <?php endwhile; ?>
+  <?php endif;
+endforeach;
+?>
 
-    <!-- 團體課程按鈕 -->
-    <?php if( $small_group_link instanceof WP_Post ): ?>
-      <?php
-        $post = $small_group_link;
-        setup_postdata($post);
-        $post_id = get_the_ID();
-      ?>
-      <a class="default-btn" href="<?php echo esc_url(home_url('/small-group#small-group-' . $post_id)); ?>">
-        團體課程
-      </a>
-      <?php wp_reset_postdata(); ?>
-    <?php endif; ?>
-
-  <?php endwhile; ?>
-<?php endif; ?>
-
-<?php if( have_rows('home_page_class_info_2') ): ?>
-  <?php while( have_rows('home_page_class_info_2') ): the_row(); 
-
-    $one_on_one_class_description = get_sub_field('1_on_1_class_description');
-    $small_group_link = get_sub_field('small_group_link');
-    
-    // 一對一課程
-    if( $one_on_one_class_description instanceof WP_Post ):
-      $post = $one_on_one_class_description; // 直接指定
-      setup_postdata($post);
-      $post_id = get_the_ID();
-      ?>
-      <article id="home-post-<?php the_ID(); ?>" <?php post_class();?>>
-        <!-- class card image -->
-        <?php
-        $image = get_field('class_image');
-        if ( $image ): ?>
-            <img src="<?php echo esc_url( $image['url'] ); ?>" alt="<?php echo esc_attr( $image['alt'] ); ?>" style="max-width:300px;" />
-        <?php endif; ?>
-
-        <!-- class card content -->
-        <h3><?php the_title(); ?></h3>
-        <?php if ( get_field('class_description') ): ?>
-          <p class="home-class-description">
-            <?php
-              $class_description = get_field('class_description');
-              $text_only = strip_tags($class_description);
-              $short_description = wp_trim_words($text_only, 60, '...');
-              echo '<div class="class-description">' . $short_description . '</div>';
-            ?>
-          </p>
-        <?php endif; ?>
-
-        <a class="default-btn" href="<?php echo esc_url(home_url('/1-on-1#1-on-1-' . $post_id)); ?>">
-          一對一課程
-        </a>
-      </article>
-      <?php wp_reset_postdata(); ?>
-    <?php endif; ?>
-
-    <!-- 團體課程按鈕 -->
-    <?php if( $small_group_link instanceof WP_Post ): ?>
-      <?php
-        $post = $small_group_link;
-        setup_postdata($post);
-        $post_id = get_the_ID();
-      ?>
-      <a class="default-btn" href="<?php echo esc_url(home_url('/small-group#small-group-' . $post_id)); ?>">
-        團體課程
-      </a>
-      <?php wp_reset_postdata(); ?>
-    <?php endif; ?>
-    
-  <?php endwhile; ?>
-<?php endif; ?>
-
-<?php if( have_rows('home_page_class_info_3') ): ?>
-  <?php while( have_rows('home_page_class_info_3') ): the_row(); 
-
-    $one_on_one_class_description = get_sub_field('1_on_1_class_description');
-    $small_group_link = get_sub_field('small_group_link');
-    
-    // 一對一課程
-    if( $one_on_one_class_description instanceof WP_Post ):
-      $post = $one_on_one_class_description; // 直接指定
-      setup_postdata($post);
-      $post_id = get_the_ID();
-      ?>
-      <article id="home-post-<?php the_ID(); ?>" <?php post_class();?>>
-        <!-- class card image -->
-        <?php
-        $image = get_field('class_image');
-        if ( $image ): ?>
-            <img src="<?php echo esc_url( $image['url'] ); ?>" alt="<?php echo esc_attr( $image['alt'] ); ?>" style="max-width:300px;" />
-        <?php endif; ?>
-
-        <!-- class card content -->
-        <h3><?php the_title(); ?></h3>
-        <?php if ( get_field('class_description') ): ?>
-          <p class="home-class-description">
-            <?php
-              $class_description = get_field('class_description');
-              $text_only = strip_tags($class_description);
-              $short_description = wp_trim_words($text_only, 60, '...');
-              echo '<div class="class-description">' . $short_description . '</div>';
-            ?>
-          </p>
-        <?php endif; ?>
-
-        <a class="default-btn" href="<?php echo esc_url(home_url('/1-on-1#1-on-1-' . $post_id)); ?>">
-          一對一課程
-        </a>
-      </article>
-      <?php wp_reset_postdata(); ?>
-    <?php endif; ?>
-
-    <!-- 團體課程按鈕 -->
-    <?php if( $small_group_link instanceof WP_Post ): ?>
-      <?php
-        $post = $small_group_link;
-        setup_postdata($post);
-        $post_id = get_the_ID();
-      ?>
-      <a class="default-btn" href="<?php echo esc_url(home_url('/small-group#small-group-' . $post_id)); ?>">
-        團體課程
-      </a>
-      <?php wp_reset_postdata(); ?>
-    <?php endif; ?>
-    
-  <?php endwhile; ?>
-<?php endif; ?>
-  <a class="default-btn" href="<?php echo esc_url(home_url('/1-on-1')); ?>">了解更多課程</a>
-  <a class="default-btn" href="<?php echo esc_url(home_url('/class-recommendation')); ?>">找尋適合我的課程</a>
+<a class="default-btn" href="<?php echo esc_url(home_url('/1-on-1')); ?>">了解更多課程</a>
+<a class="default-btn" href="<?php echo esc_url(home_url('/class-recommendation')); ?>">找尋適合我的課程</a>
 </section>
 
 <!-- Mood Section -->

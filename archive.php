cc851ef994fe798123b99adb9c560ca9b3fe7873
Yui-0807 <131344066+Yui-0807@users.noreply.github.com>
2025-06-17 21:36:53 -0700

    restructured archive.php

--- archive.php
+++ archive.php
@@ -9,7 +9,7 @@
 
 get_header();
 ?>
-
+<div class="cotent-container">
 	<main id="primary" class="site-main">
 
 		<?php if ( have_posts() ) : ?>
@@ -23,17 +23,35 @@ get_header();
 
 			<?php
 			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+			while ( have_posts() ) : the_post();?>
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+				<article 
+				id="post-<?php the_ID(); ?>" 
+				<?php post_class(); ?>
+				data-aos="fade-up"
+          		data-aos-duration="1500">
 
-			endwhile;
+					<a href="<?php the_permalink(); ?>">
+						<div class="entry-header">
+							<?php the_post_thumbnail( 'landscape-blog' ); ?>
+						</div><!-- .entry-header -->
+
+						<div class="entry-content">
+							<p class="date"><?php echo get_the_date(); ?> </p>
+							<h2><?php the_title(); ?></h2>
+							
+							<hr />
+							<?php the_excerpt(); ?>
+
+							<button class="read-post-link default-btn">繼續閱讀
+								<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M647-440H160v-80h487L423-744l57-56 320 320-320 320-57-56 224-224Z"/></svg>
+							</button>
+						</div><!-- .entry-content -->	
+
+					</a>
+				</article><!-- #post-<?php the_ID(); ?> -->
+
+			<?php endwhile;
 
 			the_posts_navigation();
 
@@ -48,4 +66,6 @@ get_header();
 
 <?php
 get_sidebar();
+
+echo "</div>";
 get_footer();

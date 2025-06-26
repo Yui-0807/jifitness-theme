02e34113de87850df53a3f1cf8fc5a4465158e2a
Yui-0807 <131344066+Yui-0807@users.noreply.github.com>
2025-06-26 15:25:14 -0700

    added testimonial-card reverse layout

--- page-testimonials.php
+++ page-testimonials.php
@@ -14,7 +14,7 @@ get_header();
 
 <main id="primary" class="site-main">
 
-<div class="modals">
+
 <!-- Modal toggle -->
 <?php
 $args = array(
@@ -27,87 +27,30 @@ if ( $query->have_posts() ) {
     while( $query->have_posts() ) {
         $query->the_post(); ?>
 
-        <div class="default-card testimonial-card">
-            <!-- Model btn -->
-            <label for="modal-<?php echo get_the_ID(); ?>" >
-            <?php if (has_post_thumbnail()) : ?>
-                <div class="testimonial-card__image">
-                <?php the_post_thumbnail('medium', ['class' => 'w-full h-auto']); ?>
-                </div>
-            <?php endif; ?>
-            <div class="testimonial-card__content">
+        <div class="testimonial-card">
+        <div class="testimonial-card_inner">
+            <div class="testimonial-text">
                 <h2><?php the_title(); ?></h2>
 
                 <div class="testimonial-card_hashtag">
-                <!-- output haastag -->
-                <?php
-                // Check rows exists.
-                if( have_rows('testimoniales_hashtag') ):
-
-                    // Loop through rows.
-                    while( have_rows('testimoniales_hashtag') ) : the_row();
-
-                        // Load sub field value.
+                    <?php if( have_rows('testimoniales_hashtag') ): while( have_rows('testimoniales_hashtag') ) : the_row();
                         $hashtag = get_sub_field('hashtag');
-                        if( $hashtag ): 
+                        if( $hashtag ):
                             $link_url = $hashtag['url'];
-                            $link_title = $hashtag['title'];?>
-                            <a 
-                            class="hashtag" 
-                            href="<?php echo esc_url( $link_url ); ?>
-                            ">
+                            $link_title = $hashtag['title']; ?>
+                            <a class="hashtag" href="<?php echo esc_url( $link_url ); ?>">
                                 <?php echo esc_html( $link_title ); ?>
                             </a>
                         <?php endif;
-
-                    // End loop.
-                    endwhile;
-                endif;?>
+                    endwhile; endif;?>
                 </div>
-                <?php the_excerpt(); ?>
+                <p><?php the_content(); ?></p>
             </div>
-            </label>
+
+            <div class="testimonial-image">
+                <?php the_post_thumbnail('medium', ['class' => 'w-full h-auto']); ?>
+            </div>
         </div>
-
-        <!-- Modal content -->
-        <input type="checkbox" id="modal-<?php echo get_the_ID(); ?>" class="modal-state">
-        <div class="modal">
-            <label for="modal-<?php echo get_the_ID(); ?>" class="modal__bg"></label>
-            <div class="modal__inner">
-                <label class="modal__close" for="modal-<?php echo get_the_ID(); ?>"></label>
-                <?php the_post_thumbnail('large'); ?>
-                
-                <div class="modal__content">
-                    <h2><?php the_title(); ?></h2>
-                    <div class="modal__hashtag">
-                    <!-- output haastag -->
-                        <?php
-                        // Check rows exists.
-                        if( have_rows('testimoniales_hashtag') ):
-
-                            // Loop through rows.
-                            while( have_rows('testimoniales_hashtag') ) : the_row();
-
-                                // Load sub field value.
-                                $hashtag = get_sub_field('hashtag');
-                                if( $hashtag ): 
-                                    $link_url = $hashtag['url'];
-                                    $link_title = $hashtag['title'];?>
-                                    <a 
-                                    class="hashtag" 
-                                    href="<?php echo esc_url( $link_url ); ?>
-                                    ">
-                                        <?php echo esc_html( $link_title ); ?>
-                                    </a>
-                                <?php endif;
-
-                            // End loop.
-                            endwhile;
-                        endif;?>
-                    </div>
-                    <?php the_content(); ?>
-                </div>
-            </div>
         </div>
 
     <?php            
@@ -115,8 +58,6 @@ if ( $query->have_posts() ) {
     wp_reset_postdata();
 } 
 ?>
-</div>
-</div>
 </main>
 <?php
 get_footer();
